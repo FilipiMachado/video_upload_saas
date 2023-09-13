99f839428030e05a19c834c0132272cfc720a6ca
filipi <filipisee@hotmail.com>
2023-09-13 09:06:17 -0300

    working on app page

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,27 @@
 import { Github } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
+import { Separator } from "./components/ui/separator";
 
 export function App() {
   return (
-    <div>
+    <div className="min-h-screen flex flex-col">
       <div className="px-6 py-3 flex items-start justify-between border-b">
         <h1 className="text-xl font-bold">FaestialUp.ai</h1>
 
         <div className="flex items-center gap-3">
           <span className="text-sm text-muted-foreground">Developed with ❤️ by Fe Rodrigues</span>
+
+          <Separator orientation="vertical" className="h-6"/>
+
           <Button variant={"outline"}>
-            <Github />
+            <Github className="w-4 h-4 mr-2"/>
             Github
           </Button>
         </div>
       </div>
+
+      <div className="bg-red-900">a</div>
     </div>
   )
 }
