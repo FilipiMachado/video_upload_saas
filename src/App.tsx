beb482cdcfd54c74ce938dbef96fb7e1d1c29ad4
filipi <filipisee@hotmail.com>
2023-09-13 08:45:08 -0300

    working on app page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
+import { Github } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+
 export function App() {
   return (
-    <div>1</div>
+    <div>
+      <div className="px-6 py-3 flex items-start justify-between border-b">
+        <h1 className="text-xl font-bold">FaestialUp.ai</h1>
+
+        <div className="flex items-center gap-3">
+          <span className="text-sm text-muted-foreground">Developed with ❤️ by Fe Rodrigues</span>
+          <Button variant={"outline"}>
+            <Github />
+            Github
+          </Button>
+        </div>
+      </div>
+    </div>
   )
 }
