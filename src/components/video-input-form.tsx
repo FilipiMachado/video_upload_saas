9f981c426078e0634acb363f90bcea5f27ffaa22
filipi <filipisee@hotmail.com>
2023-09-15 11:39:42 -0300

    structuring wasm on project

--- src/components/video-input-form.tsx
+++ src/components/video-input-form.tsx
@@ -4,29 +4,63 @@ import { Label } from "./ui/label";
 import { Textarea } from "./ui/textarea";
 import { Button } from "./ui/button";
 import { Separator } from "./ui/separator";
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, FormEvent, useMemo, useRef, useState } from "react";
 
 export const VideoInputForm = () => {
-  const [ ] = useState(null)
+  const [videoFile, setVideoFile] = useState<File | null>(null);
+  const promptInputRef = useRef<HTMLTextAreaElement>(null);
 
   function handleFileSelected(e: ChangeEvent<HTMLInputElement>) {
     const { files } = e.currentTarget;
 
     if (!files) {
+      return;
+    }
+
+    const selectedFile = files[0];
+
+    setVideoFile(selectedFile);
+  }
+
+  function handleVideoUpload(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    const prompt = promptInputRef.current?.value;
+
+    if (!videoFile) {
       return
     }
 
-    const selectedFile = files[0]
+    // convert video in audio
+    
   }
 
+  const previewUrl = useMemo(() => {
+    if (!videoFile) {
+      return null;
+    }
+
+    return URL.createObjectURL(videoFile);
+  }, [videoFile]);
+
   return (
-    <form action="space-y-6">
+    <form onSubmit={handleVideoUpload} action="space-y-6">
       <label
         htmlFor="video"
-        className="border flex rounded-md aspect-video cursor-pointer border-dashed text-sm flex-col gap-2 items-center justify-center text-muted-foreground hover:bg-primary/80 transition"
+        className="relative border flex rounded-md aspect-video cursor-pointer border-dashed text-sm flex-col gap-2 items-center justify-center text-muted-foreground hover:bg-primary/80 transition"
       >
-        <FileVideo className="w-6 h-6" />
-        Select video
+        {videoFile ? (
+          <video
+            src={previewUrl || ""}
+            controls={false}
+            className="pointer-events-none absolute inset-0"
+          />
+        ) : (
+          <>
+            <FileVideo className="w-6 h-6" />
+            Select video
+          </>
+        )}
       </label>
 
       <input
@@ -42,6 +76,7 @@ export const VideoInputForm = () => {
       <div className="space-y-2 mt-2">
         <Label htmlFor="transcription_prompt">Transcription prompt</Label>
         <Textarea
+          ref={promptInputRef}
           id="transcription_prompt"
           className="h-20 leading-relaxed resize-none"
           placeholder="Inclua palavras-chave mencionadas no vídeo separadas por vírgula (,)"
